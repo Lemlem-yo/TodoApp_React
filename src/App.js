cdeed24e2215a8c54b317ddf8f6b16f31035783c
Lemlem-yo <ltsifete@gmail.com>
2023-12-07 16:09:12 +0300

    modify the whole code what to display preetly on the css and some add functionality onthe button

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
 import './App.css';
+import React, { useState} from 'react';
 
 
 function App() {
+
+  /**When a user click the add to list button 
+   *    -display the form 
+   * This function tells that one 
+   */
+
+  const [showForm , setShowForm] = useState(false);
+
+  const handleAddButtonClick = ()=>{
+
+    setShowForm(!showForm);
+  }
+  /**
+   * To handle the default in the screen page we use other function for usablitiy
+   */
+
+  const handleFormSubmit =(event) =>{
+    event.preventDefault();
+    setShowForm(false);
+  }
   return (
     <div className = "holder">
-      <h1>Todo list</h1>
-      <div className="todo-input">
-        <label>Works: </label>
-        <input type="text" placeholder="Write the task want to do"></input>
-      <label>Description: </label>
-      <input type="text" placeholder="Write the description">
-      </input>
-      <button>Add</button>
+      <header>
+      <h1>ToDo list</h1>
+      <button onClick={handleAddButtonClick}>Add To List</button>
+      </header>
+      <div className='form-holder'>
+      {showForm && ( <form onSubmit={handleFormSubmit}>
+        <h1>Your Work list</h1>
+        <label>Works:   </label>
+        <input type="text" placeholder="Write the task you want todo"></input><br/><br/>
+        <label>Description:</label>
+        <input type="text" placeholder="Write the description"></input><br/><br/>
+        <button type="submit" >Add</button>
+      </form>
+      )}
+
       </div>
+      
+      
       <div className="task-buttons">
         <button>To Do</button>
         <button>Completed</button>
       </div>
+      
     </div>
 
-   
   );
 }
 
